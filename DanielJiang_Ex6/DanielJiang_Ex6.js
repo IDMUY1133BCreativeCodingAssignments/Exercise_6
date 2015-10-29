66f48912c7ce6386736f46ce64cf99d8aa7d7f79
Daniel Jiang <djj286@nyu.edu>
2015-10-29 03:06:04 -0400

    Daniel's Second Push
    I Tried working on it again with other people, but I'm having issues
    with p5. I would try to run it, but I would get an error that says
    "'CanvasRenderingContext2D.webkitImageSmoothingEnabled' is deprecated.
    Please use 'CanvasRenderingContext2D.imageSmoothingEnabled' instead." in
    p5.min. I tried working on the code as much as I could otherwise.

--- DanielJiang_Ex6/DanielJiang_Ex6.js
+++ DanielJiang_Ex6/DanielJiang_Ex6.js
@@ -1,5 +1,12 @@
+var Monster;
 var squad=[];
-var monsterParameters=[];
+//var sk1=[color(170,12,14),color(60, 189, 64),color(70, 112, 164),color(70, 242, 104),color(255, 202, 244)];
+//var sk2=[color(189, 110, 14),color(59, 220, 64),color(89, 0, 124),color(19, 10, 124),color(0, 255, 0)];
+//var wart=[color(111, 235, 23),color(181, 35, 203),color(11, 135, 253),(101, 255, 100),color(1, 245, 250)];
+//var x=[0,100,200,300,400];
+
+//var monsterParameters=[];
+
 var wanderX,wanderY,c,x,y;
 
 function setup(){
@@ -9,13 +16,9 @@ function setup(){
     squad[2]=Monster(200, 0, color(70, 112, 164), color(89, 0, 124), color(11, 135, 253));
     squad[3]=Monster(300, 0, color(70, 242, 104), color(19, 10, 124), color(101, 255, 100));
     squad[4]=Monster(400, 0, color(255, 202, 244), color(0, 255, 0), color(1, 245, 250));*/ //this stuff works in processing but not in here
-    monsterParameters[0]=new Monster(0, 0, color(170, 12, 14), color(189, 110, 14), color(111, 235, 23));
-    monsterParameters[1]=new Monster(100, 0, color(60, 189, 64), color(59, 220, 64), color(181, 35, 203));
-    monsterParameters[2]=new Monster(200, 0, color(70, 112, 164), color(89, 0, 124), color(11, 135, 253));
-    monsterParameters[3]=new Monster(300, 0, color(70, 242, 104), color(19, 10, 124), color(101, 255, 100));
-    monsterParameters[4]=new Monster(400, 0, color(255, 202, 244), color(0, 255, 0), color(1, 245, 250));
+    
     for(var i=0; i<5;i++){
-        squad.push(monsterParameters[i]);
+        squad.push(new Monster());
     }
 }
 
@@ -25,35 +28,47 @@ function draw(){
     Monster.head(100,100);    
     Monster.eyes(wanderX, wanderY);*/ //never again
     for(var i=0;i<squad.length;i++){
-        squad[i].monsterBody(100,100);
+        squad[i].monsterBody();
         squad[i].head(120,160);
         squad[i].eyes(wanderX,wanderY);
     }
 }
 
-function Monster(tempX,tempY,sk1,sk2,wort){
-    this.skin1=sk1;
-    this.skin2=sk2;
-    this.wort=wort;
-    this.tempX=tempX;
-    this.tempX=tempY;
+function Monster(){
+    /*for(var i=0; i<sk1.length;i++){
+        this.skin1=sk1[i];
+    }
+    for(var i=0;i<sk2.length;i++){
+        this.skin2=sk2[i];
+    }
+    for(var i=0;i<wart.length;i++){
+        this.wort=wart[i];
+    }
+    for(var i=0;i<x.length;i++){
+        this.tempX=x[i];
+    }*/
+    this.skin1=color(170,12,14);
+    this.skin2=color(60,189,64);
+    this.wort=color(70,112,164);
+    this.tempX=0;
+    this.tempY=0;
     this.w=800;
 
-    Monster.prototype.monsterBody=function(wide, tall){
+    Monster.prototype.monsterBody=function(){
         smooth();
         curve(this.w/2 - 100 + this.tempX, this.w/2-100 + this.tempY, this.w/2 - 50 + this.tempX, this.w/  2-200 + y, this.w/2 + 10 + this.tempX, this.w/2 -100 + this.tempY, this.w/2 -20 + x, this.w/2 + 20 + this.tempY);
         curve(this.w/2 + 120 + x, this.w/2-80 + y, this.w/2 + 50 + x, this.w/2-200 + y, this.w/2 - 10 + x, this.w/2 - 100 + y, this.w/2 -20 + x, this.w/2 + 40 + y); 
         fill(this.skin1);
-        ellipse(this.w/2 + x, this.w/2 + y, wide, tall); 
+        ellipse(this.w/2 + x, this.w/2 + y, 100, 100); 
         fill(this.wort);
         noStroke(); 
         ellipse(this.w/2 - 20 + x, this.w/2 + 30 + y, 20, 10);
     };
   
-    Monster.prototype.head=function(wid,hei){
+    Monster.prototype.head=function(){
         fill(this.skin2);
         stroke(0); 
-        ellipse(this.w/2 + x, this.w/2 - 120 + y, wid, hei);
+        ellipse(this.w/2 + x, this.w/2 - 120 + y, 120, 160);
         fill(0);
         rect(this.w/2 - 25 + x, this.w/2 - 105 + y, 60, 1);
         fill(255);
@@ -65,15 +80,15 @@ function Monster(tempX,tempY,sk1,sk2,wort){
         noFill();
     };
 
-    Monster.prototype.eyes=function(wX,wY){
+    Monster.prototype.eyes=function(){
         wanderX=map(mouseX, 0, width, -5, 5);
         wanderY=map(mouseY, 0, height, -5, 5); //Creates the variable that controls the eyes
         fill(193, 255, 64);
         ellipse(this.w/2 - 15 + x, this.w/2 - 120 + y, 15, 15); //left eye
         ellipse(this.w/2 + 25 + x, this.w/2 - 120 + y, 15, 15); //right eye
         fill(0);
-        ellipse(this.w/2 - 15 + wX + x, this.w/2 - 120 + wY + y, 5, 5); //left pupil
-        ellipse(this.w/2 + 25 + wX + x, this.w/2 - 120 + wY + y, 5, 5); //right pupil
+        ellipse(this.w/2 - 15 + wanderX + x, this.w/2 - 120 + wanderY + y, 5, 5); //left pupil
+        ellipse(this.w/2 + 25 + wanderX + x, this.w/2 - 120 + wanderY + y, 5, 5); //right pupil
         noFill();
     };
 }
