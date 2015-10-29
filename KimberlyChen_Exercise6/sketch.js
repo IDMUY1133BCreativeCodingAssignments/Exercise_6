1dd64d5684ce3e64b6905d8db2023d68f6319fd5
binkobon <kc2605@nyu.edu>
2015-10-29 03:42:55 -0400

    Edited

--- KimberlyChen_Exercise6/sketch.js
+++ KimberlyChen_Exercise6/sketch.js
@@ -1,4 +1,5 @@
-var bunnies = new Bunny [5];
+var bunnies = new Bunny [5]; //new array 'bunnies'
+var skin, eyes, wid, hei, x, y; //variables
 
 function setup(){
     createCanvas(1000,1000);
@@ -17,44 +18,42 @@ function draw(){
     }
 }
 
-function Bunny(newX, newY, skin1, eyeColor){
-    var skin, eyes, wid, hei, x, y;
-    var w = 300;
-    
+function Bunny(newX, newY, skin1, eyeColor){    
     this.skin = skin1;
     this.eyes = eyeColor;
     this.x = newX;
     this.y = newY;
+    this.w = 300;
     
     
    function head(wid, hei){
     fill(skin); //beige
-    ellipse(w+x, w+100+y, wid, hei); //head
-    ellipse(w-150+x, w-100+y, wid/4, hei/.75); //left ear
-    ellipse(w+150+x, w-100+y, wid/4, hei/.75); //right ear
+    ellipse(this.w+x, this.w+100+y, wid, hei); //head
+    ellipse(this.w-150+x, this.w-100+y, wid/4, hei/.75); //left ear
+    ellipse(this.w+150+x, this.w-100+y, wid/4, hei/.75); //right ear
 
     fill(255, 242, 241); //pink
-    ellipse(w-145+x, w-150+y, wid/9.8, hei/3); //ear middle L
-    ellipse(w+145+x, w-150+y, wid/9.8, hei/3); //ear middle R
+    ellipse(this.w-145+x, this.w-150+y, wid/9.8, hei/3); //ear middle L
+    ellipse(this.w+145+x, this.w-150+y, wid/9.8, hei/3); //ear middle R
 
     //facial features
     //eyes
     fill(eyes);
-    ellipse(w-100+x, w+50+y, wid/10, wid/9.5); //left eye
-    ellipse(w+100+x, w+50+y, wid/10, wid/9.5); //right eye
+    ellipse(this.w - 100 + x, this.w + 50 + this.y, wid/10, wid/9.5); //left eye
+    ellipse(this.w+100+this.x, this.w+50+this.y, wid/10, wid/9.5); //right eye
     stroke(232, 223, 208); //beige stroke
     //nose
     fill(232, 163, 146);
-    ellipse(w+x, w+80+y, wid/9.6, hei/13.3);
+    ellipse(this.w+this.x, this.w+80+this.y, wid/9.6, hei/13.3);
     //mouth
     stroke(178, 129, 85);
     strokeWeight(2);
     fill(255, 155, 137);
-    triangle(w+x, w+95+y, w-20+x, w+y+110, w+20+x, w+110+y);
+    triangle(this.w+this.x, this.w+95+this.y, this.w-20+this.x, this.w+this.y+110, this.w+20+this.x, this.w+110+this.y);
     //cheeks
     noStroke();
     fill(255, 242, 241);
-    ellipse(w-140+x, w+100+y, wid/8, hei/8);
-    ellipse(w+140+x, w+100+y, wid/8, hei/8);
+    ellipse(this.w-140+this.x, this.w+100+this.y, wid/8, hei/8);
+    ellipse(this.w+140+this.x, this.w+100+this.y, wid/8, hei/8);
    }
 }
