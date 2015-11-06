645c36a97b0479885d154ded6b8c75dbd2d8d578
poojapatel1211 <pp1553@nyu.edu>
2015-11-06 15:49:52 -0500

    Updated with loop
    I put a for loop in the draw but for some reason it is unable to detect
    the body variables in the array at the top of the code. Do you see
    something that I might have missed?

--- sketch.js
+++ sketch.js
@@ -1,20 +1,26 @@
-var body= [body1, body2, body3, body4, body5, body6];  //declaring body variables in array; 6 monsters
-
+var body = [body0, body1, body2, body3, body4, body5];  //declaring body variables in array; 6 monsters
 
 
 function setup(){
 	createCanvas(700,700);  //size(700,700)
-	body1 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),200,300,3);  //first body, random colors, location, speed
-    body2 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),250,500,5);  //second body, random colors, location, speed
-    body3 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,1);  //third body, random colors, location, speed
-    body4 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,8);  //fourth body, random colors, location, speed
-    body5 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,6);  //fifth body, random colors, location, speed
-    body6 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,1);  //sixth body, random colors, location, speed
+	body0 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),200,300,3);  //first body, random colors, location, speed
+    body1 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),250,500,5);  //second body, random colors, location, speed
+    body2 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,1);  //third body, random colors, location, speed
+    body3 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,8);  //fourth body, random colors, location, speed
+    body4 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,6);  //fifth body, random colors, location, speed
+    body5 = new Body(color(random(255),random(255),random(255)), color(random(255),random(255),random(255)), color(random(255),random(255),random(255)),50,30,1);  //sixth body, random colors, location, speed
 }
 
 
 function draw(){
 	background(255); //white background
+	for (var i = 0; i < 6 ; i++) {
+
+			body[i].move();  //calls move function
+			body[i].display(random(50,400)); //display(size)
+
+		}
+	/*
 	body1.move();  //calls move function
 	body1.display(300); //display(size)
 	body2.move();
@@ -27,8 +33,9 @@ function draw(){
 	body5.display(500);
     body6.move();
 	body6.display(340);
-}
+*/
 
+}
 
 function Body(color1,color2,color3, x, y, sp){ // the constructor
 	this.sk1 = color1;  //random
@@ -60,5 +67,5 @@ function Body(color1,color2,color3, x, y, sp){ // the constructor
 			if (this.xloc > width){
 				this.xloc= 0;
 			}
-	}
+}
 } 
